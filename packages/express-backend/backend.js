5c49d22f314e204a71a012d6c98219c58c25ea61
AndrewJ <Ajohn345@calpoly.edu>
2026-01-20 21:07:16 -0800

    Delete + Sorting

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,10 +42,39 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+const deleteUserById = (id) => {
+  const initialLength = users["users_list"].length;
+  users["users_list"] = users["users_list"].filter(
+    (user) => user["id"] !== id
+  );
+  return users["users_list"].length !== initialLength;
+};
+
 app.use(express.json());
 
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const deleted = deleteUserById(id);
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send();
+  }
+});
+
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
+  const id = req.params["id"];
   let result = findUserById(id);
   if (result === undefined) {
     res.status(404).send("Resource not found.");
@@ -56,21 +85,19 @@ app.get("/users/:id", (req, res) => {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+  let result = users["users_list"];
   if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
+    result = result.filter((user) => user["name"] === name);
   }
+  if (job != undefined) {
+    result = result.filter((user) => user["job"] === job);
+  }
+  res.send({ users_list: result });
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/users", (req, res) => {
-  res.send(users);
 });
 
 app.listen(port, () => {
